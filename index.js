f54501e14447a24f118b3d046810df06c85dcc5d
unknown <doliveirasantos98@gmail.com>
2021-07-29 18:08:53 -0300

    feat: Feito os exercícios 1,2 e 3

---
+++ index.js
@@ -0,0 +1,26 @@
+var fullName;
+var firstName = 'Diego';
+var lastName = 'Santos';
+var age = '23';
+var food = 'Pizza';
+const After = 50;
+var sentence = 'It\'s Time';
+
+fullName = firstName + ' ' + lastName;
+
+console.log(fullName);
+console.log(sentence);
+console.log(typeof(firstName));
+
+var total = 10 + 5 * 2 / 2 + 20;
+var num = 'dez';
+var uni = 'g';
+var result1 = num / 2;
+var result2 = uni * 5;
+var sum = '200' + 50;
+var increment=+ 5;
+
+console.log(result1);
+console.log(result2);
+console.log(sum);
+console.log(increment);
